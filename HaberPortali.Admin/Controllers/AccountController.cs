4280db883b983ec35ab565d202e49c260bb9146c
Nuh <hex-tr@hotmail.com>
2016-06-13 17:30:33 +0300

    loginActinFilter ve admin giriş işlemleri ok.

---
+++ HaberPortali.Admin/Controllers/AccountController.cs
@@ -0,0 +1,48 @@
+﻿using HaberPortali.Admin.Models.DTO;
+using HaberPortali.Core.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HaberPortali.Admin.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly IKullaniciRepository _kullaniciRepository;
+
+        public AccountController(IKullaniciRepository kullaniciRepository)
+        {
+            _kullaniciRepository = kullaniciRepository;
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(KullaniciVM model)
+        {
+            var kullaniciVarmi = _kullaniciRepository.GetMany(x => x.Email == model.Email && x.Sifre == model.Sifre).SingleOrDefault();
+
+            if(kullaniciVarmi!=null)
+            {
+                if(kullaniciVarmi.Rol.RolAdi=="Admin")
+                {
+                    Session["KullaniciEmail"] = kullaniciVarmi.Email;
+                    Session["Kullanici"] = kullaniciVarmi.AdSoyad;
+                    return RedirectToAction("Index","Home");
+                }
+                ViewBag.Mesaj = "Kullanıc yetkiniz admin olmalı";
+                return View();
+            }
+            ViewBag.Mesaj = "Kullanıcı bulunamadı";
+            return View();
+        }
+
+    }
+}
---
+++ HaberPortali.Admin/Models/DTO/KullaniciVM.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HaberPortali.Admin.Models.DTO
+{
+    public class KullaniciVM:BaseVM
+    {
+        [Display(Name ="Ad Soyad")]
+        [Required(ErrorMessage ="Zorunlu Alan")]
+        public string AdSoyad { get; set; }
+
+
+        [Required(ErrorMessage = "Zorunlu Alan")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Zorunlu Alan")]
+        public string Sifre { get; set; }
+
+    }
+}
